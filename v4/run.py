9de2b3ba46516a91e8a700d0154257eaba58477d
rishabhagrawal41 <rishabhagrawal41>
2020-04-15 15:41:58 +0530

    finally solved the directory issue

--- v4/run.py
+++ v4/run.py
@@ -1,16 +1,17 @@
 from os import listdir, path
 from importlib import import_module
 from time import sleep
-path = path.dirname(path.abspath(__file__))
-files = listdir(path)
+p = path.dirname(path.abspath(__file__))
+files = listdir(p)
 print(files)
 
 for i,f in enumerate(files):
     if 'reset' in f or 'template' in f or 'run' in f or 'general' in f or 'creds' in f or 'update' in f or f[-3:] != '.py': continue
-    print('\n', i, '/', len(files), f)
     try:
+        print('\n', i, '/', len(files), f)
         import_module(f[:-3])
+        print("DONEEE")
     except:
-        sleep(20)
-        import_module(f[:-3])
+        print('\n', i, '/', len(files), f)
+        __import__(f[:-3])
 
--- v4/shopify.py
+++ v4/shopify.py
@@ -11,6 +11,7 @@ from creds import col_url
 from urllib.request import urlopen
 from PIL import Image
 import xml.etree.cElementTree as ET
+from os import path
 
 sizes_debug = False
 
@@ -28,8 +29,9 @@ class Shopify():
         self.link = self.url[0].split('collections')[0] 
         self.reason = {'"Gift" in product': 0, 'No images': 0, 'Tags empty': 0, 'Women product': 0, 'Repeated product': 0, 'Sold out': 0}
         self.prods = []
-        self.csv_path = 'new/'
-        self.info_path = 'info/'
+        p = path.dirname(path.abspath(__file__))
+        self.csv_path = p + '/new/'
+        self.info_path = p + '/info/'
         self.headers  = [['Collection', 'Handle', 'Title', 'Body (HTML)', 'Vendor', 'Type', 'Tags', 'Published', 'Option1 Name', 'Option1 Value', 'Option2 Name', 'Option2 Value', 'Option3 Name', 'Option3 Value', 'Variant SKU', 'Variant Grams', 'Variant Inventory Tracker', 'Variant Inventory Policy', 'Variant Fulfillment Service', 'Variant Price', 'Variant Compare At Price', 'Variant Requires Shipping', 'Variant Taxable', 'Variant Barcode', 'Image Src', 'Image Position', 'Image Alt Text', 'Gift Card', 'SEO Title', 'SEO Description', 'Google Shopping / Google selfuct Category', 'Google Shopping / Gender', 'Google Shopping / Age Group', 'Google Shopping / MPN', 'Google Shopping / AdWords Grouping', 'Google Shopping / AdWords Labels', 'Google Shopping / Condition', 'Google Shopping / Custom Product', 'Google Shopping / Custom Label 0', 'Google Shopping / Custom Label 1', 'Google Shopping / Custom Label 2', 'Google Shopping / Custom Label 3', 'Google Shopping / Custom Label 4', 'Variant Image', 'Variant Weight Unit', 'Variant Tax Code', 'Cost per item']]
         self.rows = self.headers
         self.tot = 0
