5aa8164c0b126ba0b6c3b740f490d0357f3a8040
rishabhagrawal41 <rishabhagrawal41>
2020-05-22 16:59:39 +0530

    Changed shipping and return policy

--- v4/oliversapparel.py
+++ v4/oliversapparel.py
@@ -4,7 +4,7 @@ WEB_NAME = 'oliversapparel' # Website name
 DISPLAY_NAME = 'Olivers'
 cats = ['pants', 'shorts', 'tees', 'midlayers']
 cat_names = ['Pants', 'Shorts', 'Tees', 'Midlayers']
-shipping = 'Return is eligible within 365 days of delivery'
+shipping = ' We offer returns using our label for an $8 fee deducted from your refund, or you can return using your own carrier or a refund to store credit for no charge. Returns and exchanges must be received in original, unused and unwashed condition. All returns must be postmarked within 60 days from order date to be eligible for a return.'
 note = 'New brand'
 
 brand = Shopify(WEB_NAME, DISPLAY_NAME, cats, shipping, note)
